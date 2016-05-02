b92e67f8c9d01ab1d7e9ab30f9a3faf2a4c98b41
Jan_Hybs <jan.hybs@tul.cz>
2016-05-02 11:47:59 +0200

    Edit install script

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 # TGH install script
 
-set +x
+set -x
 
+# root perms
+find ../ -type d -exec chmod 775 {} \;
+find ../ -type f -exec chmod 664 {} \;
+chown -R jan-hybs      ../
+chgrp -R devs          ../
+
+# install scripts perms
+chmod +x install.sh
+chmod +x tgh-service
+chmod +x tgh-watchdog
+
+# allow access to data folder only devs and apache
+chmod -R 770           ../data
+chown -R www-data      ../data
+chgrp -R devs          ../data
+
+# access to jobs is for apache and tgh worker
+chmod -R 770           ../jobs
+chown -R tgh-worker    ../jobs
+chgrp -R devs          ../jobs
+
+# copy links
 cp tgh-service /usr/bin/tgh-service
 cp tgh-watchdog /usr/bin/tgh-watchdog
